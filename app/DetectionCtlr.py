1963aa8db08b087daec5e3e9998620ecd3d183ab
robert-solomon12 <20079462@mail.wit.ie>
2021-03-30 16:53:22 +0100

    Motor pin structures and functions defined

--- app/DetectionCtlr.py
+++ app/DetectionCtlr.py
@@ -1,4 +1,51 @@
-class DetectionCtl:
+#class DetectionCtl:
 
-    def __init__(self):
-        
+# initializing motors and sensors
+def init():
+    # turning off warning messages
+    gpio.setwarnings(False)
+    gpio.setmode(gpio.BOARD)
+    
+    # initializing motors
+    gpio.setup(7, gpio.OUT)
+    gpio.setup(11, gpio.OUT)
+    gpio.setup(13, gpio.OUT)
+    gpio.setup(15, gpio.OUT)
+    
+    # initializing sensors
+    gpio.setup(29,GPIO.IN) # left sensor
+    gpio.setup(31,GPIO.IN) # right sensor
+    
+    
+# turn on front left and rear left motors
+def leftMotorPairsOn():
+    gpio.output(7,1)
+    gpio.output(13,1)
+
+# turn off front left and rear left motors
+def leftMotorPairsOff():
+    gpio.output(7,0)
+    gpio.output(13,0)
+    
+    
+# turn on right front right and rear right motors
+def rightMotorPairsOn():
+    gpio.output(11,1)
+    gpio.output(15,1)
+
+
+#turn off right front right and rear right motors
+def rightMotorPairsOff():
+     gpio.output(11,0)
+     gpio.output(15,0)
+
+
+# turn off all motors
+def stopAllMotors():
+    gpio.output(7,0)
+    gpio.output(11,0)
+    gpio.output(13,0)
+    gpio.output(15,0)
+    
+# function initially called immidiately to stop all motors 
+stopAllMotors()
