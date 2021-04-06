712514f33c14421fdeb752dec4d606494f2878b5
Robert Solomon <20079462@mail.wit.ie>
2021-04-06 17:19:52 +0100

    Added some extra tests for Sonar Sensor
    Added an extra script to set up set up sonar sensor for obstacle detection for RoboCar

--- app/USense.py
+++ app/USense.py
@@ -1,23 +1,22 @@
 import RPi.GPIO as GPIO
-import time
-TRIG=12
-ECHO=16
-GPIO.setmode(GPIO.BOARD)
 
-while True:
-    print("Distance measurement in progress...")
+
+def distance(measure='cm'):
+    
+    TRIG=12
+    ECHO=16
+    GPIO.setmode(GPIO.BOARD)
+    print("distance measurement in progress")
     GPIO.setup(TRIG,GPIO.OUT)
     GPIO.setup(ECHO,GPIO.IN)
     GPIO.output(TRIG,False)
     print("waiting for Sensor to settle...")
     time.sleep(0.2)
     GPIO.output(TRIG,True)
-    
-    # waiting for 10 micro seconds
-    time.sleep(0.00001) 
+    time.sleep(0.00001)
     GPIO.output(TRIG,False)
     
-    while GPIO.input(ECHO)==0: #waiting until Echo pin is Low
+        while GPIO.input(ECHO)==0: #waiting until Echo pin is Low
         pulse_start=time.time()
     while GPIO.input(ECHO)==1: #waiting until Echo pin is High
         
@@ -26,5 +25,9 @@ while True:
     distance=pulse_duration*17150
     
     distance=round(distance,2) # rounding the distance to 2 decimal places
-    print("distance:",distance,"cm")
-    time.sleep(2)    
+        
+    GPIO.cleanup()
+    return distance
+
+print(distance('cm'))
+        
