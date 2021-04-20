56090cf466479a1ef3838af3b5e370927420e896
Robert Solomon <20079462@mail.wit.ie>
2021-04-20 23:04:28 +0100

    Added Byte extraction and updated message structures
    Added Byte extraction and serialization on additional message requests and added message structure on python client

--- app/V2GMessage.py
+++ app/V2GMessage.py
@@ -1,63 +1,4 @@
 # from Socket import *
-import socket
-import time
-
-LOCALHOST = '127.0.0.1'  # The server's hostname or IP address
-LOCALPORT = 8080       # The port used by the server
-GLOBALHOST = 'fe80::292f:2eda:aa83:cf63'  # The server's hostname or IP address
-GLOBALPORT1 = 15118 
-GLOBALPORT2 = 15443
-
-        # s.connect((HOST, PORT,0,3)) parameters to be used ...
-        
-        
-        #print('initialized Session ID in superclass')
-
-#class SupportedAppProtocolReq():
-    
-    #def __init__():
-      #  self.protocolNamespace = protocolNamespace
-      #  self.versionNumberMajor = versionNumberMajor
-      #  self.versionNumberMinor = versionNumberMinor
-      #  self.priority = priority
-        
-        # tcp connections here ...
-            
-#print('Supported App Protocol Handshake Details: ')
-
-
-# Superclass
-
-# Subclass SessionSetupReq
-
-# class SECCDiscovery():
-#     
-#     msg = [0x01,0xFE,0x90,0x00,0x00,0x00,0x00,0x02,0x10,00]
-#     
-#     buffer = bytes(msg)
-#     time.sleep(2)
-# 
-#     print ("Target IPv6 Address:", GLOBALHOST)
-#     print ("Target UDP Port:", GLOBALPORT1)
-# 
-#     sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM) # UDP
-#     time.sleep(1)
-# 
-#     print('V2G Message Details: ')
-# 
-#     time.sleep(1)
-#     
-#     print ("1. Sending SECC Discovery Req. Message: ", msg)
-#     sock.sendto(buffer, (GLOBALHOST, GLOBALPORT1,0,0));
-# 
-#   #  time.sleep(2)
-# 
-#     while True:
-#         
-#         data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-#         print ("received message from CANoe EVSE: ", data)
-
-
 
 class V2GMessage:
     
@@ -70,65 +11,110 @@ class SessionSetupReq(V2GMessage):
     def __init__(self,headerSessionId,evccid):
         super().__init__(headerSessionId)
         self.evccid = evccid
+
+
+msgReq1 = SessionSetupReq([0x01,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00],[0x02,0x00,0x00,0x00,0x00,0x01])
+
+class SessionSetupRes(V2GMessage):
+    
+    def __init__(self,headerSessionId,responseCode,evseId,evseTimestamp):
+        super().__init__(headerSessionId)
+        self.responseCode = responseCode
+        self.evseId = evseId
+        self.evseTimestamp = evseTimestamp
+          
         
-print('V2G Message Details: ')
-
-msg1 = SessionSetupReq([0x01,0x00,0x00, 0x00,0x00,0x00,0x00,0x00,0x00],[0x02,0x00,0x00,0x00,0x00,0x01])
-
-#msg1 = SessionSetupReq('100000000','020001')    
-
-
-time.sleep(2)
-
-buffer1 = msg1.headerSessionId + msg1.evccid
-
-time.sleep(2)
-b = bytes(buffer1)
-print('Header SessionId: ',msg1.headerSessionId,'EVCCID: ',msg1.evccid)
-print('bytes: ',b)
-#print('hex format: ' b.hex())
-print('Length of bytestring: ',len(b))
-print('Type of bytestring: ',type(b))
-time.sleep(1)
-
-#print(int(0x8E))
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    print('.........')
-    time.sleep(1)
-    print('.........')
-    time.sleep(1)
-    print('.........')
-    time.sleep(1)
-    s.connect((LOCALHOST, LOCALPORT))
-    print('Connected to V2G Server!')
-    time.sleep(1)
-    print('Sending Session Setup Message to OpenV2G!')
-    time.sleep(1)
-    s.sendall(b) 
-    time.sleep(2)
+class ServiceDiscoveryReq(V2GMessage):
     
-    data1 = s.recv(1024)
+    def __init__(self,headerSessionId,serviceScope,serviceCategory):
+        super().__init__(headerSessionId)
+        self.serviceScope = serviceScope
+        self.serviceCategory = serviceCategory
+
+msgReq2 = ServiceDiscoveryReq([0x02,0x29,0x63,0xB6,0x70,0x00,0xCD,0x4A,0x7B],[0x77,0x77,0x77,0x2e,0x76,0x65,0x63,0x74,0x6f,0x72,0x2e,0x63,0x6f,0x6d],[0x45,0x56,0x43,0x68,0x61,0x72,0x67,0x69,0x6e,0x67])
+
+
+class ServiceDiscoveryRes(V2GMessage):
     
-    print('Received from OpenV2G Server: ', repr(data1))
-    print('Length of data rec.: ',len(data1))
-    sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM) # TCP
-    sock.connect((GLOBALHOST, GLOBALPORT2, 0, 3))
-    print("Connected to CANoe");
-    time.sleep(2)
-    print("Sending Session Setup Message to CANoe..");
+    def __init__(self,headerSessionId,responseCode,paymentOption):
+        super().__init__(headerSessionId)
+        self.responseCode = responseCode
+        self.paymentOption = paymentOption
+       
+       
+       
+class PaymentServiceSelectionReq(V2GMessage):
     
-    bufferSessSetup = bytes(data1)
-    sock.sendall(bufferSessSetup)
+    def __init__(self,headerSessionId,serviceId,selectedPaymentOption):
+        super().__init__(headerSessionId)
+        self.serviceId = serviceId
+        self.selectedPaymentOption = selectedPaymentOption
+msgReq3 = PaymentServiceSeletionReq([])
+
+
+class PaymentServiceSelectionRes(V2GMessage):
     
-    print ("-----")
-    print ("-----")
-    print ("-----")
-    
-    while True:
-        data, addr = sock.recvfrom(2048) # buffer size is 1024 bytes
+    def __init__(self,headerSessionId,responseCode):
+        super().__init__(headerSessionId)
+        self.serviceID = serviceID
+        self.responseCode = responseCode
         
-        print ("received message from CANoe EVSE: ", data)
+        
+class PaymentDetailsReq(V2GMessage):
     
-    #time.sleep(2)
-    #s.close()
+    def __init__(self,headerSessionId,eMAID,contractSignatureCertChain):
+        super().__init__(headerSessionId)
+        self.eMAID = eMAID
+        self.contractSignatureCertChain = contractSignatureCertChain
+msgReq4 = PaymentServiceSeletionReq([])
+
+
+
+class PaymentDetailsRes(V2GMessage):
+    
+    def __init__(self,headerSessionId,responseCode):
+        super().__init__(headerSessionId)
+        self.serviceID = serviceID
+        self.responseCode = responseCode
+        
+        
+        
+
+# class ServiceDetailReq(V2GMessage):
+# 
+#     def __init__(self,headerSessionId,serviceId):
+#         super().__init__(headerSessionId)
+#         self.serviceId = serviceId
+#     
+# msgReq3 = ServiceDetailReq([]) 
+# 
+#         
+# class ServiceDetailRes(V2GMessage):
+# 
+#     def __init__(self,headerSessionId,serviceScope,serviceCategory):
+#         super().__init__(headerSessionId)
+#         self.serviceId = serviceId
+
+        
+#msgRes4 = PaymentServiceSeletionReq([])
+
+
+
+# class SessionStopReq(V2GMessage):
+#     
+#     def __init__(self,headerSessionId,serviceScope,serviceCategory):
+#         super().__init__(headerSessionId)
+#         self.serviceID = serviceID
+#         self.selectedPaymentOption = selectedPaymentOption
+#         
+# msg5 = PaymentServiceSeletionReq([])
+# 
+# 
+# 
+# class SessionStopRes(V2GMessage):
+#     
+#     def __init__(self,headerSessionId,evccid):
+#         super().__init__(headerSessionId)
+#         self.evccid = evccid
+#         
+# msg5 = PaymentServiceSeletionReq([])
